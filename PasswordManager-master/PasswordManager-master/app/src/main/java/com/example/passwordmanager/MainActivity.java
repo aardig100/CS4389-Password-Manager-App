946f147b1bf20fe82043ad183b02d0f71c8b3224
aidennguyen22 <74615848+aidennguyen22@users.noreply.github.com>
2021-10-21 18:52:07 -0500

    Add design files

---
+++ PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.passwordmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.app.Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void register(View v) {
+        Intent intent = new Intent(this, NewUserRegistration.class);
+        startActivity(intent);
+    }
+
+
+}
