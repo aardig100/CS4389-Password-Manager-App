5ec39c5550e4c57bac5f44add34517ea1e592059
Lizan Ahmetovic <34887879+lizan859@users.noreply.github.com>
2021-11-09 23:14:33 -0600

    Add files via upload

--- PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -1,30 +1,84 @@
-package com.example.passwordmanager;
+package com.example.mypasswordsafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username, password;
+    private Button login;
+    SharedPreferences sharedPreferences;
+
+    public static final String mypreference ="mypref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        login = (Button)findViewById(R.id.login);
+        //instantiating shared preferences
+        sharedPreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        if(sharedPreferences.contains(String.valueOf(username)))
+        {
+            username.setText(sharedPreferences.getString(String.valueOf(username), ""));
+        }
+        if(sharedPreferences.contains(String.valueOf(password)))
+        {
+            password.setText(sharedPreferences.getString(String.valueOf(password),""));
+        }
     }
 
-    public void login(View v) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+    public void validate(View view) {
+
+        String name = username.getText().toString();
+        String passwd= password.getText().toString();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(String.valueOf(username), name);
+        editor.putString(String.valueOf(password),passwd);
+        editor.commit();
+
+        if(name.equals("Zaina") || passwd.equals("gohome")){
+            Intent intent = new Intent(this, MainActivity.class);
+        }else{
+            Toast.makeText(this, "Password/Username incorrect", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void Get(View view)
+    {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        sharedPreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        if(sharedPreferences.contains(String.valueOf(username)))
+        {
+            username.setText(sharedPreferences.getString(String.valueOf(username), ""));
+        }
+        if(sharedPreferences.contains(String.valueOf(password)))
+        {
+            password.setText(sharedPreferences.getString(String.valueOf(password),""));
+        }
     }
 
-    public void register(View v) {
-        Intent intent = new Intent(this, NewUserRegistration.class);
-        startActivity(intent);
+
+
+    public Button getLogin() {
+        return login;
     }
 
-
+    public void setLogin(Button login) {
+        this.login = login;
+    }
 }
