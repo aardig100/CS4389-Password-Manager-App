610e810010fe492e296fcac870a09153b3a40dce
Lizan Ahmetovic <34887879+lizan859@users.noreply.github.com>
2021-11-09 23:41:02 -0600

    update to second activity
    added shared preferences to

--- PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/SecondActivity.java
+++ PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/SecondActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 import java.util.concurrent.Executor;
 import androidx.annotation.NonNull;
@@ -22,16 +23,36 @@ public class SecondActivity extends AppCompatActivity {
     private Executor exec;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
-
+    //for storing email and passwords
+    private EditText email, password;
+    
+    SharedPreferences sharedPreferences;
+    public static final String mypreference ="mypref";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        //storage UI 
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+                                           
         //init UI view
         authStatusTv = findViewById(R.id.authStatusTv);
         authBtn = findViewById(R.id.authBtn);
 
+        //setting shared preferences
+        sharedPreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        if(sharedPreferences.contains(String.valueOf(email)))
+        {
+            email.setText(sharedPreferences.getString(String.valueOf(email), ""));
+        }
+        if(sharedPreferences.contains(String.valueOf(password)))
+        {
+            password.setText(sharedPreferences.getString(String.valueOf(password),""));
+        }
+        
         //init biometric
         exec = ContextCompat.getMainExecutor(this);
         biometricPrompt = new BiometricPrompt(SecondActivity.this, exec, new BiometricPrompt.AuthenticationCallback(){
