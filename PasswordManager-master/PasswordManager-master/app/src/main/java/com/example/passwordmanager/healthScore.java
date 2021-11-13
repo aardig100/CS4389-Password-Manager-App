4dc9e1df61c4107511724dbfdd6141d967cbebc6
Daljin <Herediae30@gmail.com>
2021-11-12 21:23:09 -0600

    fixed. it was all me and not orobosa :)

--- PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/healthScore.java
+++ PasswordManager-master/PasswordManager-master/app/src/main/java/com/example/passwordmanager/healthScore.java
@@ -1,47 +1,38 @@
 package com.example.passwordmanager;
 
 public class healthScore {
-    public static void main(String[] args) {
 
-        //test password
-        //String password = "Ferr@rizzz";
 
-        //System.out.print(calculateHealth(password));
+    public String calculateHealth(String password){
 
-    }
-
-    public static String calculateHealth(String password) {
         // overall score of password
         int score = 0;
 
 
-        if( password.length() < 8 ) {
-            return "0"; // bad password, return 0
-        }
-        else if ( password.length() >= 10 ) {
-            score += 1;
+        if( password.length() < 10 ) {
+            return "0/5"; // bad password, return 0
         }
         else {
             score += 1;
         }
 
         // if password contains one digit, add 1 to total score
-        if( password.matches("(?=.[0-9]).") ) {
+        if( password.matches("(?=.*[0-9]).*") ) {
             score += 1;
         }
 
         // if password contains one lower case letter, add 1 to total score
-        if( password.matches("(?=.[a-z]).") ) {
+        if( password.matches("(?=.*[a-z]).*") ) {
             score += 1;
         }
 
         // if password contains one upper case letter, add 1 to total score
-        if( password.matches("(?=.[A-Z]).") ) {
+        if( password.matches("(?=.*[A-Z]).*") ) {
             score += 1;
         }
 
         // if password contains one special character, add 1 to total score
-        if( password.matches("(?=.[~!@#$%^&()_-]).*") ) {
+        if( password.matches("(?=.*[~!@#$%^&*()_-]).*") ) {
             score += 1;
         }
 
